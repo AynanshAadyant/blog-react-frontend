1372ea24e433e115bac4665fbceb5776a3b0708b
AynanshAadyant <aynaadyant@gmail.com>
2024-11-21 23:56:16 +0530

    .env initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import './App.css'
 
 function App() {
-
+  const react_app_appwrite = import.meta.env.VITE_APPWRITE_URL 
+  console.log( react_app_appwrite )
   return (
     <>
       <h1> This is the initial state </h1>
