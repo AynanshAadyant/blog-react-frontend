22fe7f0ea3b9ce73b9bf46605022281b248b9b5d
AynanshAadyant <aynaadyant@gmail.com>
2024-11-22 12:02:03 +0530

    Actions - appwrite configuration complete, redux configuration completed, components = basic header, footer, basic react-router-dom configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,45 @@
-import React from 'react'
-import config from "./config/config"
+import React, {useEffect, useState} from 'react'
+import {useDispatch} from 'react-redux'
+import authService from "./appwrite/auth"
+import { login, logout } from "./store/authSlice"
+import Header from "./components/header/Header"
+import Footer from "./components/footer/Footer"
+import {Outlet} from 'react-router-dom'
 
 function App() {
-  console.log( config.appwriteURL )
-  return (
-    <>
-      <h1> This is the initial state </h1>
-    </>
+
+  const [ loading, setLoading ] = useState( true )
+  const dispatch  = useDispatch()   
+  useEffect( () => {
+    authService.getCurrentUser()
+    .then( (userData) => {
+        if( userData )
+        {
+          dispatch( login( {userData}))
+        }
+        else{
+          dispatch( logout( {userData} ))
+        }
+      }
+    )
+    .finally(
+      () => setLoading(  false )
+    )
+  }, [])
+
+  
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap justify-center">
+      <div className="bg-gray-400">
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+      </div>
+    </div>
+  ) : (
+    <h1 className='text-2xl'> Not loading </h1>
   )
 }
 
